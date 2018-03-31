59e6f69501af4981c46c731242cc157fe5c78d63
Alexandre BEAUVOIS <abeauvoi@student.42.fr>
2018-03-31 21:42:59 +0200

    scripts 01 and 02

--- system/setup.sh
+++ system/setup.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-mkdir /Volumes/Storage/goinfre/$USER/vagrant
-echo 'export VAGRANT_HOME=/Volumes/Storage/goinfre/$USER/vagrant' >> ~/.zshrc
-rm Vagrantfile
+VAGRANT_HOME="/Volumes/Storage/goinfre/$USER/vagrant"
+
+if ! [[ -d "$VAGRANT_HOME" ]];
+then 
+	mkdir /Volumes/Storage/goinfre/$USER/vagrant
+fi
+
+grep -Fxq "export VAGRANT_HOME=$VAGRANT_HOME" ~/.zshrc > /dev/null || echo "export VAGRANT_HOME=$VAGRANT_HOME" >> ~/.zshrc
+
+rm -f Vagrantfile
 vagrant init debian/stretch64
-while ! [ -f ./Vagrantfile ];
+
+while ! [[ -f ./Vagrantfile ]];
 do
 	sleep 1;
 done;
+
 sed -i '' -e "s:\# \(config.vm.synced_folder\).*:\1 \".\", \"/git\":" Vagrantfile
 
 vagrant up
